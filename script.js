e2ec16a013c39831771b0f7123a7cd675acc137e
Alifia Rahmah <alifiarahmah03@gmail.com>
2023-09-06 01:01:00 +0700

    add back to top button

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-const toParagraph = (text) => {
-  return `<p>${text}</p>`;
-};
-
-const toParagraphs = (text) => {
-  return text.split("\n").map(toParagraph).join("");
-};
-
-const toH2 = (text) => {
-  return `<h2>${text}</h2>`;
-};
-
-const toH3 = (text) => {
-  return `<h3>${text}</h3>`;
-};
-
-const toOl = (list) => {
-  return `<ol>${list.map((item) => `<li>${item}</li>`).join("")}</ol>`;
+const backToTop = () => {
+  window.scrollTo({ top: 0, behavior: "smooth" });
 };
