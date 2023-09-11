2cd76e0d5fa075348db5268443e4823742eaa328
Alifia Rahmah <alifiarahmah03@gmail.com>
2023-09-12 00:19:45 +0700

    make nav button for mobile

---
+++ assets/posts.js
@@ -0,0 +1,92 @@
+const posts = [
+  {
+    id: "tentang-kucing",
+    title: "Mengenal Kucing",
+    thumbnail:
+      "https://images.unsplash.com/photo-1604398907654-ec2bf7108b4a?auto=format&fit=crop&h=500",
+    source: "Kucing - Wikipedia bahasa Indonesia",
+    source_url: "https://id.wikipedia.org/wiki/Kucing",
+    contents: [
+      {
+        element: "p",
+        child:
+          "Kucing disebut juga kucing domestik atau kucing rumah (nama ilmiah: <i>Felis silvestris catus</i> atau <i>Felis catus</i>) adalah sejenis mamalia karnivora dari keluarga <i>Felidae</i>. Saat ini, kucing adalah salah satu hewan peliharaan terpopuler di dunia. Kucing juga merupakan jenis hewan peliharaan yang banyak diminati karena tingkah lakunya yang lucu.",
+      },
+      {
+        element: "p",
+        child:
+          'Kucing peliharaan atau kucing rumah adalah salah satu predator terhebat di dunia. Kucing ini dapat membunuh atau memakan beberapa ribu spesies, kucing besar biasanya kurang dari 100. Akan tetapi, karena ukurannya yang kecil, kucing tidak begitu berbahaya bagi manusia. Kucing dianggap sebagai "karnivora yang sempurna" dengan gigi dan saluran pencernaan yang khusus. Gigi premolar dan molar pertama membentuk sepasang taring di setiap sisi mulut yang bekerja efektif seperti gunting untuk merobek daging.',
+      },
+      {
+        element: "p",
+        child:
+          'Meskipun memiliki reputasi sebagai hewan penyendiri, kucing biasanya dapat membentuk koloni liar, tetapi tidak menyerang dalam kelompok seperti singa. Setiap kucing memiliki daerahnya sendiri dan selalu terdapat daerah "netral" di mana para kucing dapat saling mengawasi atau bertemu tanpa adanya konflik teritorial atau agresi.',
+      },
+      {
+        element: "p",
+        child:
+          "Kucing termasuk hewan yang bersih. Mereka sering merawat diri dengan menjilati rambut mereka. Saliva atau air liur mereka adalah agen pembersih yang kuat, tetapi dapat memicu alergi pada manusia. Kadang kala kucing memuntahkan hairball atau gulungan rambut yang terkumpul di dalam perut mereka. Kucing menyimpan energi dengan cara tidur lebih sering ketimbang hewan lain. Lama tidur kucing bervariasi antara 12—16 jam per hari, dengan angka rata-rata 13—14 jam. Akan tetap, tidak jarang dijumpai kucing yang tidur selama 20 jam dalam satu hari.",
+      },
+    ],
+  },
+  {
+    id: "kucing-lebih-baik",
+    title: "5 Alasan Harus Memelihara Kucing",
+    thumbnail:
+      "https://static.promediateknologi.id/crop/0x0:0x0/0x0/webp/photo/jawapos/2023/02/photo_2023-02-22_12-29-18.jpg",
+    source: "Kompas.com",
+    source_url:
+      "https://www.jawapos.com/hobi-kesenangan/012904186/dikenal-memberikan-banyak-manfaat-berikut-5-alasan-harus-memelihara-kucing?page=all",
+    contents: [
+      {
+        element: "p",
+        child: `Kucing menjadi salah satu hewan yang seringkali dijadikan peliharaan oleh kebanyakan orang. Bagaimana tidak, kucing adalah hewan yang mudah beradaptasi dengan lingkungan tempat tinggal.`,
+      },
+      {
+        element: "p",
+        child: `Sebagian besar orang memelihara kucing untuk menghilangkan rasa kesepian, seperti diajak bermain atau bahkan berinteraksi. Karena kucing dikenal sebagai hewan yang mudah untuk bonding dengan manusia. Untuk itu, dengan memelihara kucing dapat memberikan manfaat bagi yang merawatnya. Seperti yang dilansir dari petguard.co.uk, simak 5 alasan mengapa harus memelihara kucing.`,
+      },
+      {
+        element: "h3",
+        child: "1. Perawatannya mudah",
+      },
+      {
+        element: "p",
+        child:
+          "Sejatinya, merawat kucing bukanlah hal yang sulit untuk dilakukan. Pun dengan kebutuhan dalam pemeliharaannya juga tidak terlalu membutuhkan biaya yang besar, terutama jika dibandingkan merawat hewan lainnya.",
+      },
+      {
+        element: "h3",
+        child: "2. Dapat menurunkan stres",
+      },
+      {
+        element: "p",
+        child: `Sebuah penelitian menunjukkan bahwa membelai hewan peliharaan dapat membantu mengurangi jumlah kortisol dalam tubuh manusia. Semakin banyak kortisol yang dimiliki, maka akan semakin stres dan cemas. Untuk itu, memelihara hewan seperti kucing adalah pilihan yang tepat untuk menurunkan stres.`,
+      },
+      {
+        element: "h3",
+        child: "3. Hewan yang mandiri",
+      },
+      {
+        element: "p",
+        child: `Kucing adalah salah satu hewan peliharaan yang lebih mandiri dibandingkan dengan anjing. Mereka tidak dibiakkan dengan menghabiskan banyak waktu bersama manusia, melainkan lebih memilih menyesuaikan diri dengan kehidupan di sekitar. Ini yang membuat kucing menjadi hewan yang mandiri.`,
+      },
+      {
+        element: "h3",
+        child: "4. Bisa menjadi peneman di saat sepi",
+      },
+      {
+        element: "p",
+        child: `Pasalnya, bermain bersama kucing dapat mencegah rasa kesepian dan membantu meningkatkan suasana hati. Sering kali orang memelihara kucing bukan hanya wujudnya yang menggemaskan, tetapi karena berinteraksi dengan kucing adalah hiburan baginya.`,
+      },
+      {
+        element: "h3",
+        child: "5. Berumur panjang",
+      },
+      {
+        element: "p",
+        child: `Kucing mampu bertahan hidup hingga umur 20 tahun, meskipun ada beberapa faktor lainnya yang dapat mempengaruhi umur kucing. Inilah mengapa banyak orang yang lebih memilih untuk memelihara kucing daripada hewan lain.`,
+      },
+    ],
+  },
+];
--- script.js
+++ script.js
@@ -1,3 +1,47 @@
+// populate #navigation
+const navigation = document.querySelector("#navigation");
+let navigationText = ``;
+posts.forEach((post) => {
+  navigationText += `
+    <a href="#${post.id}">${post.title}</a>
+  `;
+});
+navigationText += `<a href="#tentang-penulis" class="hide-in-desktop">Tentang Penulis</a>`;
+navigation.innerHTML = navigationText;
+
+const navToggle = document.querySelector("#nav-toggle");
+const toggleNav = () => {
+  navigation.style.display = navigation.style.display === "flex" ? "none" : "flex";
+}
+navToggle.addEventListener("click", toggleNav);
+
+// populate #content
+const content = document.querySelector("#content");
+let contentText = ``;
+posts.forEach((post) => {
+  contentText += `
+    <article id="${post.id}">
+      <h2>${post.title}</h2>
+  `;
+  if (post.thumbnail) {
+    contentText += `
+      <img src="${post.thumbnail}">
+    `;
+  }
+  post.contents.forEach((content) => {
+    contentText += `
+      <${content.element}>${content.child}</${content.element}>
+    `;
+  });
+  contentText += `
+    </article>
+  `;
+});
+content.innerHTML = contentText;
+
+// set backToTopButton button
+const backToTopButton = document.querySelector("#back-to-top");
 const backToTop = () => {
   window.scrollTo({ top: 0, behavior: "smooth" });
 };
+backToTopButton.addEventListener("click", backToTop);
